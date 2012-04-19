bf372450e75f0b34df938ffa899fce5eff610867
showp1984 <showp1984@gmail.com>
2012-04-19 18:25:25 +0200

    cpufreq: initial badass commit
    * Feautures:
      - Based on Ondemand Governor
      - 2 and 3 Phase Functionality
      - GPU Control Algorithm
    * Modifications:
      - Upstreamed to Ondemand Governor in this tree
      - Samsung Hackery Removed from upstreaming
      - Tuned 2 and 3 Phase Freqs for JF
      - Tuned GPU Control for Adreno 320
    
    Change-Id: If4a43d13246bf91fc44a86d9f730c667fa595cd4
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -23,6 +23,10 @@
 
 #define CPUFREQ_NAME_LEN 16
 
+#ifdef CONFIG_CPU_FREQ_GOV_BADASS_GPU_CONTROL
+/* Badass gpu state detection */
+extern bool gpu_busy_state;
+#endif
 
 /*********************************************************************
  *                     CPUFREQ NOTIFIER INTERFACE                    *
@@ -423,6 +427,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
+extern struct cpufreq_governor cpufreq_gov_badass;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_badass)
 #endif
 
 
