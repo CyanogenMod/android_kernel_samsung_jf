1173eb767fbbd9a549e462df85229b2bde941483
Mike Kasick <mike@kasick.org>
2012-07-07 19:00:47 -0400

    Add and enable kexec hardboot support.
    Consists of squashed commits from:
    https://github.com/mkasick/android_kernel_samsung_jfltespr.git
    
    commit 750bb80f2854d6af5273e55ea179a4c60b2d9efc
    Author: Mike Kasick <mike@kasick.org>
    Date:   Sun Jun 2 23:11:24 2013 -0400
    
        Clear download mode flag on kexec hardboot.
    
    commit 138ba851ee949af291eae914f410aea2d85ed9f5
    Author: Mike Kasick <mike@kasick.org>
    Date:   Sun May 12 22:39:26 2013 -0400
    
        Support hard booting to a kexec kernel.
    
        See KEXEC_HARDBOOT config option help for details.
    
    commit 3ab41019a7af08395c233bebb605c2f1ea49c8e0
    Author: Mike Kasick <mike@kasick.org>
    Date:   Sat Jul 7 23:10:24 2012 -0400
    
        Support copying of kernel tagged list (atags) in the decompressor.
    
        This is needed to hardboot kexec a kernel with a new tags list (including a
        new kernel command line), since the new atags would otherwise be lost with
        the limited kernel memory mapping.  Without this patch, a hardboot-kexec'd
        kernel uses the atags provided by the bootloader.
    
    commit 446c3d7a6631877f7dc9142359cf0eae300e9a08
    Author: Mike Kasick <mike@kasick.org>
    Date:   Sat Jul 7 23:09:39 2012 -0400
    
        Enable caching and buffering for all of physical RAM in the decompressor.
    
        Old method is to enable caching and buffering only for the 256 MB at the
        start of the decompressor image.  This makes a hardboot-kexec'd kernel very
        slow to decompress since the decompressor is located far above the kernel
        destination.  This patch reduces boot time from 35 to 8 seconds on epicmtd.
    
    commit d3646be88f5b1f9a8ae714c922ec50c681b7157f
    Author: Mike Kasick <mike@kasick.org>
    Date:   Sat Jul 7 19:00:47 2012 -0400
    
        Enable and fix kexec syscall support.
    
        Use mem_text_write_kernel_word when assigning reboot_code_buffer parameters
        to avoid protection faults (writes to read-only kernel memory) when
        CONFIG_STRICT_MEMORY_RWX is enabled.
    
    Change-Id: I0400ad00fcf02b8ad017f8aa371724a659b930b4

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -40,6 +40,10 @@
 #include "msm_watchdog.h"
 #include "timer.h"
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/kexec.h>
+#endif
+
 #define WDT0_RST	0x38
 #define WDT0_EN		0x40
 #define WDT0_BARK_TIME	0x4C
@@ -373,6 +377,23 @@ static struct notifier_block dload_reboot_block = {
 };
 #endif
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+void msm_kexec_hardboot(void)
+{
+#if defined(CONFIG_MSM_DLOAD_MODE) && !defined(CONFIG_SEC_DEBUG)
+	/* Do not enter download mode on reboot. */
+	set_dload_mode(0);
+#endif
+
+	/* Set PM8XXX PMIC to reset on power off. */
+	pm8xxx_reset_pwr_off(1);
+
+	/* Reboot with the recovery kernel since the boot kernel decompressor may
+	 * not support the hardboot jump. */
+	__raw_writel(0x77665502, restart_reason);
+}
+#endif
+
 static int __init msm_pmic_restart_init(void)
 {
 	int rc;
@@ -427,6 +448,9 @@ static int __init msm_restart_init(void)
 	restart_reason = MSM_IMEM_BASE + RESTART_REASON_ADDR;
 #endif
 	pm_power_off = msm_power_off;
+#ifdef CONFIG_KEXEC_HARDBOOT
+	kexec_hardboot_hook = msm_kexec_hardboot;
+#endif
 
 	return 0;
 }
