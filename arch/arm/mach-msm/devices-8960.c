7ad8ff977a892e1d289305f15588921d48ab6994
Carter Cooper <ccooper@codeaurora.org>
2012-10-28 20:54:17 -0600

    msm: kgsl: Add separate GPU shader memory mapping
    Add separate kernel memory mapping for GPU shader memory.
    Previously, both the register and the shader memory were mapped
    as one entity, with the mapping length being equal to combined
    size of register memory and shader memory. Now, we separate the
    shader memory mapping to help in cases of GPU devices where the
    shader and register memory may not be adjacent. This helps to
    dump the shader memory in the postmortem snapshot. By having a
    separate mapping for shader memory, the snapshot dump routine
    simply reads in the shader memory range specified and dumps.
    
    Change-Id: I30e59dffe849fefcfbbaf0e5245cc1024ccedcb1
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>
    
    msm: Fix mem leak when using per-process pages
    
    When GPU is using per process page tables the code creates
    a new page table (domain) for every process. There is also
    some associated structures allocated to keep track of each
    domain. However, when cleaning up the memory used for a page
    table only the domain is freed and not the associated structures.
    
    Add a function to allow a client to properly unregister a domain
    which will free all the memory associated with a domain.
    Call the unregister function from kgsl driver code.
    
    CRs-fixed: 480801
    Change-Id: I9704c12c66a95ce66cea63b619bbdbafa0f1eac8
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    
    Conflicts:
    	arch/arm/mach-msm/iommu_domains.c
    
    msm: kgsl: Remove nap_allowed variable and functionality
    
    Allow NAP on all targets.  The splitting of the clock enable
    and disable calls into enable/prepare and disable/unprepare
    allows us to safely make this change for all targets.
    
    Change-Id: I03d909b86aef33631a887d159cf0a807a6d0ae75
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -3368,7 +3368,30 @@ struct msm_bus_scale_pdata grp2d1_bus_scale_pdata = {
 };
 #endif
 
-static struct resource kgsl_3d0_resources[] = {
+struct resource kgsl_3d0_resources_8960ab[] = {
+	{
+		.name = KGSL_3D0_REG_MEMORY,
+		.start = 0x04300000, /* GFX3D address */
+		.end = 0x0430ffff,
+		.flags = IORESOURCE_MEM,
+	},
+	{
+		.name = KGSL_3D0_SHADER_MEMORY,
+		.start = 0x04310000, /* Shader Mem Address (8960AB) */
+		.end = 0x0431ffff,
+		.flags = IORESOURCE_MEM,
+	},
+	{
+		.name = KGSL_3D0_IRQ,
+		.start = GFX3D_IRQ,
+		.end = GFX3D_IRQ,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+
+int kgsl_num_resources_8960ab = ARRAY_SIZE(kgsl_3d0_resources_8960ab);
+
+static struct resource kgsl_3d0_resources_8960[] = {
 	{
 		.name = KGSL_3D0_REG_MEMORY,
 		.start = 0x04300000, /* GFX3D address */
@@ -3439,7 +3462,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.num_levels = ARRAY_SIZE(grp3d_freq) + 1,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
-	.nap_allowed = true,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE | KGSL_CLK_MEM_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
 	.bus_scale_table = &grp3d_bus_scale_pdata,
@@ -3452,8 +3474,8 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 struct platform_device msm_kgsl_3d0 = {
 	.name = "kgsl-3d0",
 	.id = 0,
-	.num_resources = ARRAY_SIZE(kgsl_3d0_resources),
-	.resource = kgsl_3d0_resources,
+	.num_resources = ARRAY_SIZE(kgsl_3d0_resources_8960),
+	.resource = kgsl_3d0_resources_8960,
 	.dev = {
 		.platform_data = &kgsl_3d0_pdata,
 	},
@@ -3506,7 +3528,6 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.num_levels = ARRAY_SIZE(grp2d_freq) + 1,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
-	.nap_allowed = true,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
 	.bus_scale_table = &grp2d0_bus_scale_pdata,
@@ -3573,7 +3594,6 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.num_levels = ARRAY_SIZE(grp2d_freq) + 1,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
-	.nap_allowed = true,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
 	.bus_scale_table = &grp2d1_bus_scale_pdata,
