e9d771eb241c6d36ef36dce9ceb493b9eba4d945
Lucille Sylvester <lsylvest@codeaurora.org>
2013-01-18 17:09:02 -0700

    msm: kgsl: Add intermediate power levels
    Make use of the two independent power domains to
    add intermediate levels.  Attempt to just increase
    the bus frequency before also raising the GPU
    frequency.
    
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>
    
    Conflicts:
    	arch/arm/boot/dts/msm8974-gpu.dtsi
    	arch/arm/boot/dts/msm8974-v2.dtsi
    	drivers/gpu/msm/kgsl_pwrctrl.c
    	drivers/gpu/msm/kgsl_pwrctrl.h
    
    Change-Id: Ibe85d1c02941430c5da83a75a09d5bc0ec5f8ec0

--- arch/arm/mach-msm/include/mach/kgsl.h
+++ arch/arm/mach-msm/include/mach/kgsl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -22,7 +22,7 @@
 #define KGSL_CLK_AXI	0x00000020
 #define KGSL_CLK_ALT_MEM_IFACE 0x00000040
 
-#define KGSL_MAX_PWRLEVELS 5
+#define KGSL_MAX_PWRLEVELS 10
 
 #define KGSL_CONVERT_TO_MBPS(val) \
 	(val*1000*1000U)
@@ -83,6 +83,7 @@ struct kgsl_device_platform_data {
 	bool strtstp_sleepwake;
 	unsigned int clk_map;
 	unsigned int idle_needed;
+	unsigned int step_mul;
 	struct msm_bus_scale_pdata *bus_scale_table;
 	struct kgsl_device_iommu_data *iommu_data;
 	int iommu_count;
