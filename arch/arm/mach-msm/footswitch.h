02ce0384fd8fea3397dce254d8a9f4129530969d
paris_yeh <paris_yeh@asus.com>
2013-02-04 11:38:10 +0800

    msm: footswitch-8x60: Allow specification of per-fs reset delays
    Custom reset rates can be specified for clocks via fs_clk_data. If
    these rates are very low, then the default reset delay of 1us may
    not be sufficient. Address this by allowing custom reset delays to
    be specified on a per-footswitch bases.
    
    Change-Id: Id9b9377d79436a7fe5007a8f37216a456d66ce1a
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    
    msm: footswitch-8x60: Update 8064 footswitch_enable() sequence for GFX3D
    
    Update 8064's GFX3D enable sequence per hardware designer recommendations
    to improve system stability when responding to the inrush current event.
    
    Differences from the standard footswitch_enable() sequence include:
     - Decreasing the reset_rate from 27MHz to 1.8MHz, and increasing
       reset_delay_us from 1us to 10us to compensate for the slower clock.
     - Enable clocks for reset propagation only after the core has been
       powered on.
     - Forcefully limit the AFAB and EBI1 clocks to a low value (decided
       by the RPM) by means of a write to the RPM_CTL RPM resource.
     - Removal of the reset toggle after powering on the core, which (while
       harmless) is not applicable to 8064.
    
    Change-Id: I943842b56ff96b2e6077a419566d91ac184a6fda
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    
    msm: footswitch-8x60: Skip GFX memory power collapse on 8064.
    
    Update footswitch_disable to skip GFX memory power collapse on 8064 to avoid
    any voltage droop on vddmx during GFX3D footswitch toggling.
    
    As per hardware designer recommendations this would help to improve system
    stability when responding to the inrush current event.
    
    Change-Id: Ifa2f62629f0639d29f882d009dc5f4c10da46ca8
    Signed-off-by: Shashank Mittal <mittals@codeaurora.org>
    Signed-off-by: Ed Tam <etam@google.com>

--- arch/arm/mach-msm/footswitch.h
+++ arch/arm/mach-msm/footswitch.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2013 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -28,7 +28,8 @@
 #define FS_VFE		8
 #define FS_VPE		9
 #define FS_VCAP		10
-#define MAX_FS		11
+#define FS_GFX3D_8064	11
+#define MAX_FS		12
 
 struct fs_clk_data {
 	const char *name;
@@ -41,6 +42,7 @@ struct fs_clk_data {
 struct fs_driver_data {
 	int bus_port0, bus_port1;
 	struct fs_clk_data *clks;
+	unsigned long reset_delay_us;
 };
 
 #define FS_GENERIC(_drv_name, _id, _name, _dev_id, _data) \
