17a37c75d33dac6bd9f63ae687c82acfcc8701db
Deepak Verma <dverma@codeaurora.org>
2013-02-12 16:55:21 +0530

    msm: vidc: Add support for NV21 color format encoding
    This change adds NV21 color format encoding support.
    
    CRs-fixed: 448511
    Change-Id: If26f1241a006e2d325668b8e189f6ff76df8e125
    Signed-off-by: Deepak Verma <dverma@codeaurora.org>

--- drivers/video/msm/vidc/common/enc/venc_internal.c
+++ drivers/video/msm/vidc/common/enc/venc_internal.c
@@ -123,6 +123,10 @@ u32 vid_enc_set_get_inputformat(struct video_client_ctx *client_ctx,
 			format.buffer_format =
 				VCD_BUFFER_FORMAT_NV12_16M2KA;
 			break;
+		case VEN_INPUTFMT_NV21_16M2KA:
+			format.buffer_format =
+				VCD_BUFFER_FORMAT_NV21_16M2KA;
+			break;
 		default:
 			status = false;
 			break;
@@ -151,6 +155,9 @@ u32 vid_enc_set_get_inputformat(struct video_client_ctx *client_ctx,
 				break;
 			case VCD_BUFFER_FORMAT_TILE_4x2:
 				*input_format = VEN_INPUTFMT_NV21;
+				break;
+			case VCD_BUFFER_FORMAT_NV21_16M2KA:
+				*input_format = VEN_INPUTFMT_NV21_16M2KA;
 				break;
 			default:
 				status = false;
