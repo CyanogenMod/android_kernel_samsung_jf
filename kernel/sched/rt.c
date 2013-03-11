73bf740e5685f2be0c904f8ae4e0f024ad8ee730
Steve Muckle <smuckle@codeaurora.org>
2013-03-11 16:33:42 -0700

    sched: provide per cpu-cgroup option to notify on migrations
    On systems where CPUs may run asynchronously, task migrations
    between CPUs running at grossly different speeds can cause
    problems.
    
    This change provides a mechanism to notify a subsystem
    in the kernel if a task in a particular cgroup migrates to a
    different CPU. Other subsystems (such as cpufreq) may then
    register for this notifier to take appropriate action when
    such a task is migrated.
    
    The cgroup attribute to set for this behavior is
    "notify_on_migrate" .
    
    Change-Id: Ie1868249e53ef901b89c837fdc33b0ad0c0a4590
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1619,6 +1619,7 @@ static int push_rt_task(struct rq *rq)
 	struct task_struct *next_task;
 	struct rq *lowest_rq;
 	int ret = 0;
+	bool moved = false;
 
 	if (!rq->rt.overloaded)
 		return 0;
@@ -1688,6 +1689,7 @@ retry:
 
 	deactivate_task(rq, next_task, 0);
 	set_task_cpu(next_task, lowest_rq->cpu);
+	moved = true;
 	activate_task(lowest_rq, next_task, 0);
 	ret = 1;
 
@@ -1697,6 +1699,11 @@ retry:
 
 out:
 	put_task_struct(next_task);
+
+	if (moved && task_notify_on_migrate(next_task))
+		atomic_notifier_call_chain(&migration_notifier_head,
+					   cpu_of(lowest_rq),
+					   (void *)cpu_of(rq));
 
 	return ret;
 }
@@ -1711,8 +1718,10 @@ static void push_rt_tasks(struct rq *rq)
 static int pull_rt_task(struct rq *this_rq)
 {
 	int this_cpu = this_rq->cpu, ret = 0, cpu;
-	struct task_struct *p;
+	struct task_struct *p = NULL;
 	struct rq *src_rq;
+	bool moved = false;
+	int src_cpu = 0;
 
 	if (likely(!rt_overloaded(this_rq)))
 		return 0;
@@ -1773,6 +1782,10 @@ static int pull_rt_task(struct rq *this_rq)
 			deactivate_task(src_rq, p, 0);
 			set_task_cpu(p, this_cpu);
 			activate_task(this_rq, p, 0);
+
+			moved = true;
+			src_cpu = cpu_of(src_rq);
+
 			/*
 			 * We continue with the search, just in
 			 * case there's an even higher prio task
@@ -1783,6 +1796,11 @@ static int pull_rt_task(struct rq *this_rq)
 skip:
 		double_unlock_balance(this_rq, src_rq);
 	}
+
+	if (moved && task_notify_on_migrate(p))
+		atomic_notifier_call_chain(&migration_notifier_head,
+					   this_cpu,
+					   (void *)src_cpu);
 
 	return ret;
 }
