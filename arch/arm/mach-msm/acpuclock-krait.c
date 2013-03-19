2c8d27abd327d364af793dd8661339c8c46d96e6
Matt Wagantall <mattw@codeaurora.org>
2013-03-18 21:30:16 -0700

    msm: acpuclock-krait: Remove inapplicable 8064 errata workaround
    The secondary MUX switching errata is worked around in the secure
    warm-boot path, and does not require an in-kernel workaround.
    
    For devices with older warm-boot code (without the workaround),
    the removal of the kernel workaround is also safe. The errata
    symptoms can only manifest themselves after an L2 power-collapse,
    which can't occur until after all Krait CPUs have been brought
    online at least once. Since the secondary MUX is only touched by
    acpuclock when onlining CPUs for the first time, the MUX will
    never be switched after L2 power-collapse.
    
    Change-Id: If675cfab7f325a9f7af6e06ff5ed67bd65461f20
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -48,7 +48,6 @@
 #define PRI_SRC_SEL_HFPLL	1
 #define PRI_SRC_SEL_HFPLL_DIV2	2
 
-#define SECCLKAGD		BIT(4)
 #ifdef CONFIG_SEC_DEBUG_SUBSYS
 int boost_uv;
 int speed_bin;
@@ -84,20 +83,10 @@ static void __cpuinit set_sec_clk_src(struct scalable *sc, u32 sec_src_sel)
 {
 	u32 regval;
 
-	/* 8064 Errata: disable sec_src clock gating during switch. */
 	regval = get_l2_indirect_reg(sc->l2cpmr_iaddr);
-	regval |= SECCLKAGD;
-	set_l2_indirect_reg(sc->l2cpmr_iaddr, regval);
-
-	/* Program the MUX */
 	regval &= ~(0x3 << 2);
 	regval |= ((sec_src_sel & 0x3) << 2);
 	set_l2_indirect_reg(sc->l2cpmr_iaddr, regval);
-
-	/* 8064 Errata: re-enabled sec_src clock gating. */
-	regval &= ~SECCLKAGD;
-	set_l2_indirect_reg(sc->l2cpmr_iaddr, regval);
-
 	/* Wait for switch to complete. */
 	mb();
 	udelay(1);
