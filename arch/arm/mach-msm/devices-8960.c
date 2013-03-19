21bfcb40d5eb812a0cace676a1887f9e30f6fe17
Laura Abbott <lauraa@codeaurora.org>
2013-03-19 10:18:16 -0700

    msm: Remove android_pmem.h from board and devices files
    CONFIG_ANDROID_PMEM is completely deprecated. Remove all references
    to the android pmem header.
    
    Change-Id: Ic68a3af6b9cd01a439a841af26ae6dba09db2911
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -21,7 +21,6 @@
 #include <linux/avtimer.h>
 #include <asm/clkdev.h>
 #include <mach/kgsl.h>
-#include <linux/android_pmem.h>
 #include <mach/irqs-8960.h>
 #include <mach/dma.h>
 #include <linux/dma-mapping.h>
