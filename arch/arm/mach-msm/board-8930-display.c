2e30b8fb377a479e460dc84f69c48646e08193ce
Mukesh Jha <cmjha@codeaurora.org>
2013-05-13 07:55:36 -0700

    msm_fb: hdmi: change HDMI_PHY_REG1 based on foundry.
    Set value of HDMI_PHY_REG1[2:0] for output voltage swing control,
    to prevent fallout for Vlow for GF foundry MSM's.
    
    Change-Id: Ifb0f694226896fa1ad74dd84fb489f015901fc3e
    Signed-off-by: Abhishek Kharbanda <akharban@codeaurora.org>
    Signed-off-by: Mukesh Jha <cmjha@codeaurora.org>

--- arch/arm/mach-msm/board-8930-display.c
+++ arch/arm/mach-msm/board-8930-display.c
@@ -532,6 +532,7 @@ static int hdmi_core_power(int on, int show);
 static int hdmi_cec_power(int on);
 static int hdmi_gpio_config(int on);
 static int hdmi_panel_power(int on);
+static bool hdmi_platform_foundry(void);
 
 static struct msm_hdmi_platform_data hdmi_msm_data = {
 	.irq = HDMI_IRQ,
@@ -540,6 +541,7 @@ static struct msm_hdmi_platform_data hdmi_msm_data = {
 	.cec_power = hdmi_cec_power,
 	.panel_power = hdmi_panel_power,
 	.gpio_config = hdmi_gpio_config,
+	.foundry = hdmi_platform_foundry,
 };
 
 static struct platform_device hdmi_msm_device = {
@@ -798,6 +800,26 @@ static int hdmi_cec_power(int on)
 error:
 	return rc;
 }
+
+static int hdmi_panel_power(int on)
+{
+	int rc;
+
+	pr_debug("%s: HDMI Core: %s\n", __func__, (on ? "ON" : "OFF"));
+	rc = hdmi_core_power(on, 1);
+	if (rc)
+		rc = hdmi_cec_power(on);
+
+	pr_debug("%s: HDMI Core: %s Success\n", __func__, (on ? "ON" : "OFF"));
+	return rc;
+}
+
+static bool hdmi_platform_foundry(void)
+{
+	return cpu_is_msm8930ab() ? true : false ;
+}
+
+
 #endif /* CONFIG_FB_MSM_HDMI_MSM_PANEL */
 
 void __init msm8930_init_fb(void)
