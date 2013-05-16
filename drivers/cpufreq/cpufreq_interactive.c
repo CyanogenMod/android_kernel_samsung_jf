fd623e758fcae7633f7756ed55c9a40727d7ec08
Viresh Kumar <viresh.kumar@linaro.org>
2013-05-16 14:58:53 +0530

    cpufreq: interactive: Move definition of cpufreq_gov_interactive downwards
    This moves definition of cpufreq_gov_interactive towards the bottom of file, so
    that we don't have to add prototype of cpufreq_governor_interactive() in the
    beginning of file.
    
    Change-Id: I04bd1004954eb36502c5cd7e35d3d7274cddaf95
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -126,19 +126,6 @@ static bool io_is_busy;
  */
 static unsigned int max_freq_hysteresis;
 
-static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
-		unsigned int event);
-
-#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE
-static
-#endif
-struct cpufreq_governor cpufreq_gov_interactive = {
-	.name = "interactive",
-	.governor = cpufreq_governor_interactive,
-	.max_transition_latency = 10000000,
-	.owner = THIS_MODULE,
-};
-
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 						  cputime64_t *wall)
 {
@@ -1203,6 +1190,16 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 	}
 	return 0;
 }
+
+#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE
+static
+#endif
+struct cpufreq_governor cpufreq_gov_interactive = {
+	.name = "interactive",
+	.governor = cpufreq_governor_interactive,
+	.max_transition_latency = 10000000,
+	.owner = THIS_MODULE,
+};
 
 static void cpufreq_interactive_nop_timer(unsigned long data)
 {
