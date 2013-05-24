34e06bd6032093e412576a903e13b4eb0199f7ac
satyavaraprasad yerramsetti <satyav@codeaurora.org>
2013-05-24 12:14:32 +0530

    msm: camera: Handle locks properly
    Update camera drivers to unlock mutex while returning
    the function in caseof errors.
    
    Change-Id: Iaac7ae3a0337445ee072c10c69080c24ed6e512c
    Signed-off-by: satyavaraprasad yerramsetti <satyav@codeaurora.org>

--- drivers/media/video/msm/msm.c
+++ drivers/media/video/msm/msm.c
@@ -217,6 +217,7 @@ static int msm_camera_v4l2_reqbufs(struct file *f, void *pctx,
 		pmctl = msm_cam_server_get_mctl(pcam->mctl_handle);
 		if (pmctl == NULL) {
 			pr_err("%s Invalid mctl ptr", __func__);
+			mutex_unlock(&pcam_inst->inst_lock);
 			return -EINVAL;
 		}
 		pmctl->mctl_vbqueue_init(pcam_inst, &pcam_inst->vid_bufq,
@@ -948,8 +949,10 @@ static int msm_open(struct file *f)
 	D("%s Inst %p use_count %d\n", __func__, pcam_inst, pcam->use_count);
 	if (pcam->use_count == 1) {
 		server_q_idx = msm_find_free_queue();
-		if (server_q_idx < 0)
-			return server_q_idx;
+		if (server_q_idx < 0) {
+			pr_err("%s No free queue available ", __func__);
+			goto msm_cam_server_begin_session_failed;
+		}
 		rc = msm_server_begin_session(pcam, server_q_idx);
 		if (rc < 0) {
 			pr_err("%s error starting server session ", __func__);
@@ -958,7 +961,7 @@ static int msm_open(struct file *f)
 		pmctl = msm_cam_server_get_mctl(pcam->mctl_handle);
 		if (!pmctl) {
 			pr_err("%s mctl ptr is null ", __func__);
-			goto msm_cam_server_begin_session_failed;
+			goto msm_cam_server_get_mctl_failed;
 		}
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
 		pmctl->client = msm_ion_client_create(-1, "camera");
@@ -1018,18 +1021,17 @@ msm_send_open_server_failed:
 		pmctl->mctl_cmd = NULL;
 	}
 mctl_open_failed:
-	if (pcam->use_count == 1) {
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
-		if (ion_client_created) {
-			D("%s: destroy ion client", __func__);
-			kref_put(&pmctl->refcount, msm_release_ion_client);
-			ion_client_created = 0;
-		}
-#endif
-		if (msm_server_end_session(pcam) < 0)
-			pr_err("%s: msm_server_end_session failed\n",
-				__func__);
+	if (ion_client_created) {
+		D("%s: destroy ion client", __func__);
+		kref_put(&pmctl->refcount, msm_release_ion_client);
+		ion_client_created = 0;
 	}
+#endif
+msm_cam_server_get_mctl_failed:
+	if (msm_server_end_session(pcam) < 0)
+		pr_err("%s: msm_server_end_session failed\n",
+			__func__);
 msm_cam_server_begin_session_failed:
 	open_fail_flag = 0;
 	if (pcam->use_count == 1) {
@@ -1288,6 +1290,7 @@ copy_from_user_failed:
 payload_alloc_fail:
 	kfree(event_qcmd);
 event_qcmd_alloc_fail:
+	mutex_unlock(&pcam->event_lock);
 	return rc;
 }
 
