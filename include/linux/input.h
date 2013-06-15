3f91b05a20d88019b30aff4ffec66e66c16fb049
David Herrmann <dh.herrmann@gmail.com>
2013-06-15 15:32:44 +0200

    input: document gamepad API and add extra keycodes
    Until today all gamepad input drivers report their data differently. It is
    nearly impossible to write applications for more than one device in a
    generic way. Therefore, this patch introduces a uniform gamepad API which
    will be used for all new drivers.
    
    Instead of mapping buttons by their labels, we now map them by position.
    This allows applications to work with any gamepad regardless of the labels
    on the buttons. Furthermore, we standardize the ABS_* codes for analog
    triggers and sticks.
    
    For D-Pads the long overdue BTN_DPAD_* codes are introduced. They should
    be fairly obvious how to use. To avoid confusion, the action buttons now
    have BTN_EAST/SOUTH/WEST/NORTH aliases.
    
    Reported-by: Todd Showalter <todd@electronjump.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    Conflicts:
    	include/uapi/linux/input.h
    
    Change-Id: If65177b7e1abb6868a9042ad1d31c47cdf0ed882
    
    Conflicts:
    	include/linux/input.h

--- include/linux/input.h
+++ include/linux/input.h
@@ -513,10 +513,14 @@ struct input_keymap_entry {
 
 #define BTN_GAMEPAD		0x130
 #define BTN_A			0x130
+#define BTN_SOUTH		0x130
 #define BTN_B			0x131
+#define BTN_EAST		0x131
 #define BTN_C			0x132
 #define BTN_X			0x133
+#define BTN_NORTH		0x133
 #define BTN_Y			0x134
+#define BTN_WEST		0x134
 #define BTN_Z			0x135
 #define BTN_TL			0x136
 #define BTN_TR			0x137
@@ -711,6 +715,11 @@ struct input_keymap_entry {
 
 #define KEY_DMB_ANT_DET_UP		0x21b
 #define KEY_DMB_ANT_DET_DOWN		0x21c
+
+#define BTN_DPAD_UP		0x220
+#define BTN_DPAD_DOWN		0x221
+#define BTN_DPAD_LEFT		0x222
+#define BTN_DPAD_RIGHT		0x223
 
 #define BTN_TRIGGER_HAPPY		0x2c0
 #define BTN_TRIGGER_HAPPY1		0x2c0
