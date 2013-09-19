e8f64c7243f735254bb5a6095c5456690d33cfec
Narsinga Rao Chella <nrchella@codeaurora.org>
2013-09-18 18:47:42 -0700

    ASoC: msm: qdsp6: Add support to configure min and max rates
    Add support to configure min and max frame rates for EVRC/EVRCB/
    EVRCWB/EVRCNW codecs. This change is needed to support variable
    length frames for VoIP calls.
    
    Change-Id: Id17dd645b17b1b8c1dc21afcf20dbc7e2c9c0d30
    Signed-off-by: Narsinga Rao Chella <nrchella@codeaurora.org>
    [zhaoweiliew: Backported from the qdsp6v2 subsystem by arco]
    Signed-off-by: Zhao Wei Liew <zhaoweiliew@gmail.com>

--- sound/soc/msm/qdsp6/q6voice.h
+++ sound/soc/msm/qdsp6/q6voice.h
@@ -906,6 +906,8 @@ struct mvs_driver_info {
 	ul_cb_fn ul_cb;
 	dl_cb_fn dl_cb;
 	void *private_data;
+	uint32_t evrc_min_rate;
+	uint32_t evrc_max_rate;
 };
 
 struct incall_rec_info {
@@ -1012,7 +1014,9 @@ void voc_register_mvs_cb(ul_cb_fn ul_cb,
 void voc_config_vocoder(uint32_t media_type,
 			uint32_t rate,
 			uint32_t network_type,
-			uint32_t dtx_mode);
+			uint32_t dtx_mode,
+			uint32_t evrc_min_rate,
+			uint32_t evrc_max_rate);
 
 enum {
 	DEV_RX = 0,
