d51cb258178ea8e65a2142cd715be90e37f0cf9a
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-29 16:56:45 -0700

    sched: Introduce CONFIG_SCHED_FREQ_INPUT
    Introduce a compile time flag to enable scheduler guidance of
    frequency selection. This flag is also used to turn on or off
    window-based load stats feature.
    
    Having a compile time flag will let some platforms avoid any
    overhead that may be present with this scheduler feature.
    
    Change-Id: Id8dec9839f90dcac82f58ef7e2bd0ccd0b6bd16c
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1358,6 +1358,8 @@ static inline void update_cfs_rq_blocked_load(struct cfs_rq *cfs_rq,
 					      int force_update) {}
 #endif
 
+#ifdef CONFIG_SCHED_FREQ_INPUT
+
 static inline unsigned int task_load(struct task_struct *p)
 {
 	return p->ravg.demand;
@@ -1390,6 +1392,8 @@ void init_new_task_load(struct task_struct *p)
 	for (i = 0; i < RAVG_HIST_SIZE; ++i)
 		p->ravg.sum_history[i] = 0;
 }
+
+#endif /* CONFIG_SCHED_FREQ_INPUT */
 
 static void enqueue_sleeper(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
