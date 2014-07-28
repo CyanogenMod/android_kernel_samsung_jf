d808a280e5b6598b8f59968cd33166e2b55aaa82
Dan Pasanen <dan.pasanen@gmail.com>
2014-07-28 08:13:51 -0500

    msm2: sync with upstream
    Change-Id: I6f3321cd18829dc762e7729f315d5e16e26dac15

--- drivers/gpu/msm2/kgsl_pwrctrl.c
+++ drivers/gpu/msm2/kgsl_pwrctrl.c
@@ -34,10 +34,6 @@
 #define UPDATE_BUSY_VAL		1000000
 #define UPDATE_BUSY		50
 
-#ifdef CONFIG_MSM_KGSL_KERNEL_API_ENABLE
-struct device *stored_dev;
-#endif
-
 /*
  * Expected delay for post-interrupt processing on A3xx.
  * The delay may be longer, gradually increase the delay
@@ -646,52 +642,6 @@ static int kgsl_pwrctrl_reset_count_show(struct device *dev,
 	return snprintf(buf, PAGE_SIZE, "%d\n", device->reset_counter);
 }
 
-#ifdef CONFIG_MSM_KGSL_KERNEL_API_ENABLE
-int kgsl_pwrctrl_min_pwrlevel_store_kernel(int level)
-{
-	struct device *dev = stored_dev;
-	struct kgsl_device *device;
-	int request_level = level;
-	char buf_level[2] = {0,};
-
-	if (!dev) {
-		printk("%s, dev is null\n", __func__);
-		return -EINVAL;
-	}
-
-	device = kgsl_device_from_dev(dev);
-
-	if (!device) {
-		printk("%s, fail to get device\n", __func__);
-		return -EINVAL;
-	}
-
-	if (request_level < 0) {
-		printk("%s, invalid level : %d\n", __func__, request_level);
-		return -EINVAL;
-	}
-
-	if (request_level > device->pwrctrl.num_pwrlevels - 2)
-		request_level = device->pwrctrl.num_pwrlevels - 2;
-
-	buf_level[0] = (char)(request_level + '0');
-
-	return kgsl_pwrctrl_min_pwrlevel_store(dev, NULL, buf_level, sizeof(buf_level));
-}
-
-int kgsl_pwrctrl_num_pwrlevels_show_kernel(void)
-{
-
-	struct kgsl_device *device = kgsl_device_from_dev(stored_dev);
-	struct kgsl_pwrctrl *pwr;
-	if (device == NULL)
-		return 0;
-
-	pwr = &device->pwrctrl;
-	return pwr->num_pwrlevels - 1;
-}
-#endif
-
 static void __force_on(struct kgsl_device *device, int flag, int on)
 {
 	if (on) {
@@ -853,9 +803,6 @@ static const struct device_attribute *pwrctrl_attr_list[] = {
 
 int kgsl_pwrctrl_init_sysfs(struct kgsl_device *device)
 {
-#ifdef CONFIG_MSM_KGSL_KERNEL_API_ENABLE
-	stored_dev = device->dev;
-#endif
 	return kgsl_create_device_sysfs_files(device->dev, pwrctrl_attr_list);
 }
 
