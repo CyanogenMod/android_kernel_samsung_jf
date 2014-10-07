e43ea500d2e0a9eb68ce814105b6ea34ed404252
Dan Pasanen <dan.pasanen@gmail.com>
2014-10-07 15:05:27 -0500

    jf: update camera driver from I9505GUEUCNF3
    Change-Id: I07f0e1748ac97cc2dd066cade6fd68cf9de99e55

--- include/media/msm_isp.h
+++ include/media/msm_isp.h
@@ -242,7 +242,6 @@
 #define VFE_CMD_STOP_RECORDING_DONE                     165
 #define VFE_CMD_RGB_ALL_CFG                             166
 #define VFE_CMD_RGB_ALL_UPDATE                          167
-#define VFE_CMD_MAX                                     168
 
 struct msm_isp_cmd {
 	int32_t  id;
