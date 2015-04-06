6e78cb518bb20453455cdc55eef7eae764421f13
Junjie Wu <junjiew@codeaurora.org>
2015-04-06 11:16:45 -0700

    cpufreq: interactive: Don't set floor_validate_time during boost
    Frequency selection algorithm guarantees its chosen frequency
    is not lower than hispeed_freq as long as boost is enabled.
    
    Setting floor_freq and floor_validate_time during boost could block
    CPU frequency from going below hispeed_freq even after
    boostpulse_duration expires, if min_sample_time is higher than
    boostpulse_duration. This conflicts with the intention of commit
    de091367ead15b6e95dd1d0743a18f0da5a07ee5
    (cpufreq: interactive: specify duration of CPU speed boost pulse)
    to allow CPU to ramp down immediately after boost expires. It also
    makes boost behavior inconsistent since it depends on min_sample_time.
    
    Avoid setting floor_freq and floor_validate_time when boost starts.
    
    Change-Id: I12852998af46cfbfaf8661eb5e8d5301b6f631e7
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -688,14 +688,6 @@ static void cpufreq_interactive_boost(void)
 				ktime_to_us(ktime_get());
 			anyboost = 1;
 		}
-
-		/*
-		 * Set floor freq and (re)start timer for when last
-		 * validated.
-		 */
-
-		pcpu->floor_freq = hispeed_freq;
-		pcpu->floor_validate_time = ktime_to_us(ktime_get());
 	}
 
 	spin_unlock_irqrestore(&speedchange_cpumask_lock, flags);
