719c30b4b3ab2d49adc436637f71c5f1ab7eb7b2
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-27 17:47:13 -0500

    jf: set proper compass calibration point for vzw model
    Change-Id: I4634d9cdb436efca15a3698402a87a8d2995551e

--- arch/arm/mach-msm/board-jf_eur.c
+++ arch/arm/mach-msm/board-jf_eur.c
@@ -2150,7 +2150,9 @@ static void ssp_get_positions(int *acc, int *mag)
 	else
 		*acc = MPU6500_BOTTOM_RIGHT_UPPER;
 
-	if (system_rev > BOARD_REV06)
+	if (system_rev == BOARD_REV11)
+		*mag = YAS532_TOP_RIGHT_LOWER;
+	else if (system_rev > BOARD_REV06)
 		*mag = YAS532_BOTTOM_RIGHT_LOWER;
 	else if (system_rev > BOARD_REV03)
 		*mag = YAS532_TOP_RIGHT_LOWER;
