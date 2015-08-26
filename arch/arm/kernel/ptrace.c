4e428beb10434f1b8366eaba2bbdf40997ecaa64
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-25 21:32:11 -0500

    Revert "CHROMIUM: arch/arm: move secure_computing into trace; respect return code"
    This reverts commit a651024793c3fc24d486a950c56e52bebc8397b6.
    
    Change-Id: Ic21913b9ca930244de4cc52f5bbc2939793bcae7
    (cherry picked from commit a25e94221e7b9023a3c1ee606897f2d5ee20a3db)
    (cherry picked from commit 487905d4b3ec2c31b16ca7abb63814908cc5c9a5)

--- arch/arm/kernel/ptrace.c
+++ arch/arm/kernel/ptrace.c
@@ -910,16 +910,12 @@ long arch_ptrace(struct task_struct *child, long request,
 asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 {
 	unsigned long ip;
-	current_thread_info()->syscall = scno;
 
 	if (why)
 		audit_syscall_exit(regs);
-	else {
-		if (secure_computing(scno) == -1)
-			return -1;
+	else
 		audit_syscall_entry(AUDIT_ARCH_ARM, scno, regs->ARM_r0,
 				    regs->ARM_r1, regs->ARM_r2, regs->ARM_r3);
-	}
 
 	if (why == 0 && test_and_clear_thread_flag(TIF_SYSCALL_RESTARTSYS))
 		scno = __NR_restart_syscall - __NR_SYSCALL_BASE;
@@ -928,12 +924,7 @@ asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 	if (!(current->ptrace & PT_PTRACED))
 		return scno;
 
-	/*
-	 * IP is used to denote syscall entry/exit:
-	 * IP = 0 -> entry, =1 -> exit
-	 */
-	ip = regs->ARM_ip;
-	regs->ARM_ip = why;
+	current_thread_info()->syscall = scno;
 
 	/*
 	 * IP is used to denote syscall entry/exit:
