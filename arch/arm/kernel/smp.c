5b86a320bb1612c4223700ae76f28877cdbb2d0a
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-11-25 22:16:58 +0800

    ARM: smp: Remove last remnants of idle task
    This was part of the hotplug thread infrastructure patch series mismerge.
    
    Change-Id: I997f7ddb1090ffc1c57e2b5f299567622b787e44

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -370,9 +370,6 @@ void __init smp_cpus_done(unsigned int max_cpus)
 
 void __init smp_prepare_boot_cpu(void)
 {
-	unsigned int cpu = smp_processor_id();
-
-	per_cpu(cpu_data, cpu).idle = current;
 }
 
 void __init smp_prepare_cpus(unsigned int max_cpus)
