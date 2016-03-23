b056a5699c922f0df7f6864493b26911122188aa
Jaiju Yesudasan <cjaijuy@codeaurora.org>
2016-03-23 15:51:54 +0530

    msm: clock: Fix potential NULL pointer dereference
    Pointer 'b->ctl_reg' may be dereferenced at function
    __branch_disable_reg()
    
    Change-Id: I5655389e3bab06feb56a5851019fe4cf19f0a93c
    Signed-off-by: Jaiju Yesudasan <cjaijuy@codeaurora.org>

--- arch/arm/mach-msm/clock-local.c
+++ arch/arm/mach-msm/clock-local.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2009-2012,2016 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -352,7 +352,7 @@ u32 __branch_disable_reg(const struct branch *b, const char *name)
 	u32 reg_val;
 
 	reg_val = b->ctl_reg ? readl_relaxed(b->ctl_reg) : 0;
-	if (b->en_mask) {
+	if (b->en_mask && b->ctl_reg) {
 		reg_val &= ~(b->en_mask);
 		writel_relaxed(reg_val, b->ctl_reg);
 	}
