64141e31ddc2d4ec48ea5844fc7e0f9b51210a02
Paul Keith <javelinanddart@gmail.com>
2016-07-04 13:44:00 -0700

    fscrypto: Fix Linux 3.4 compatibility
    * Fix include path
    * Fix missing includes
    * Fix function parameters for Linux 3.4
    * Thanks to @Rashed97 for the help
    
    Change-Id: I3163f25534da983b42b43fdb0c389daf48e6fdd3
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- fs/crypto/fname.c
+++ fs/crypto/fname.c
@@ -15,6 +15,7 @@
 #include <keys/encrypted-type.h>
 #include <keys/user-type.h>
 #include <linux/crypto.h>
+#include <linux/module.h>
 #include <linux/scatterlist.h>
 #include <linux/ratelimit.h>
 #include <linux/fscrypto.h>
