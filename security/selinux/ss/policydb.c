a73e005aa908cb649b6ace532f2793c4897721c3
kushan02 <kushan.mehta@yahoo.com>
2016-08-15 10:43:12 +0530

    Revert fix for selinux
    Change-Id: I724eece3798e248c13a67bc2a2633b60ea873612

--- security/selinux/ss/policydb.c
+++ security/selinux/ss/policydb.c
@@ -2000,19 +2000,7 @@ static int filename_trans_read(struct policydb *p, void *fp)
 		if (rc)
 			goto out;
 
-		rc = hashtab_insert(p->filename_trans, ft, otype);
-		if (rc) {
-			/*
-			 * Do not return -EEXIST to the caller, or the system
-			 * will not boot.
-			 */
-			if (rc != -EEXIST)
-				goto out;
-			/* But free memory to avoid memory leak. */
-			kfree(ft);
-			kfree(name);
-			kfree(otype);
-		}
+		hashtab_insert(p->filename_trans, ft, otype);
 	}
 	hash_eval(p->filename_trans, "filenametr");
 	return 0;
