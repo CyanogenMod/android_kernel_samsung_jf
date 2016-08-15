188572bbeff769dfc5abb87b6afe93d462f17632
kushan02 <kushan.mehta@yahoo.com>
2016-08-15 11:02:37 +0530

    small change in cputime.h
    Change-Id: Icc615e82b62a2b6b37a973edf56144b5cab56c69

--- include/asm-generic/cputime.h
+++ include/asm-generic/cputime.h
@@ -15,6 +15,7 @@ typedef u64 __nocast cputime64_t;
 
 #define cputime64_to_jiffies64(__ct)	(__force u64)(__ct)
 #define jiffies64_to_cputime64(__jif)	(__force cputime64_t)(__jif)
+#define cputime64_sub(__a, __b)    ((__a) - (__b)) 
 
 #define nsecs_to_cputime64(__ct)	\
 	jiffies64_to_cputime64(nsecs_to_jiffies64(__ct))
