24f408745001033e38b2c43dff06bfd01b96ced3
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-02 16:59:18 +0800

    motor: immvibespi: Set default intensity to max
    Set the default vibrator intensity to that of stock,
    which is the max intensity.
    
    Change-Id: Ie24d8afb2aca5fefdb9451dc83154b174f44a34a

--- drivers/motor/immvibespi.c
+++ drivers/motor/immvibespi.c
@@ -36,7 +36,7 @@
 
 #define LEVEL_MAX           100
 #define LEVEL_MIN           0
-#define LEVEL_DEFAULT       50
+#define LEVEL_DEFAULT       LEVEL_MAX
 
 /*
 ** This SPI supports only one actuator.
